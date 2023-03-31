279b442ade7e2ec36260414af0917da9ea0e78be
Sri-1227 <srinivasmekala1227@gmail.com>
2023-03-31 17:13:07 +0530

    more changes

--- host/settings.py
+++ host/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'website/static']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles/']
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
