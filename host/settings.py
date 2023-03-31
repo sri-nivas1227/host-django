2516f5f7c93eeb9bd579719800f8e0f3e258d29b
Sri-1227 <srinivasmekala1227@gmail.com>
2023-03-31 16:56:59 +0530

    hosting setup added

--- host/settings.py
+++ host/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-asm)^9fqc3()n5%+$q%pg^13=0zlq#d#)*(k5@2b6ju9ko$fm_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ""]
 
 
 # Application definition
